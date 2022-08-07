f95e6d065f546e023dad469855f84b07c1499fca
jfernandesdev <jfernandes.dev@gmail.com>
2022-08-06 21:03:47 -0300

    STYLE: header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,9 @@
-import { gql, useQuery } from '@apollo/client'
-
-const GET_LESSONS_QUERY = gql`
-  query {
-    lessons {
-      id
-      title
-
-      teacher {
-        name
-        bio
-      }
-    }
-  }
-`
-interface Lesson {
-  id: string
-  title: string
-  teacher: {
-    name: string
-    bio: string
-  }
-}
+import { Event } from './pages/Event'
 
 export function App() {
-  const { data } = useQuery(GET_LESSONS_QUERY)
-
   return (
     <>
-      <h1 className="title">Hello World!</h1>
-
-      <ul>
-        {data?.lessons.map((lesson: Lesson) => (
-          <li key={lesson.id}>{lesson.title}</li>
-        ))}
-      </ul>
+      <Event />
     </>
   )
 }
---
+++ src/pages/Event.tsx
@@ -0,0 +1,15 @@
+import { Header } from '../components/Header'
+import { Sidebar } from '../components/Sidebar'
+import { Video } from '../components/Video'
+import { Lesson } from '../components/Lesson'
+
+export function Event() {
+  return (
+    <>
+      <Header />
+      <Sidebar />
+      <Video />
+      <Lesson />
+    </>
+  )
+}
