333ae2775e81a6cd10647234a8277b75b65f80de
jfernandesdev <jfernandes.dev@gmail.com>
2022-08-07 14:28:51 -0300

    STYLE: subscribe page

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,12 +1,12 @@
 import { Route, Routes } from 'react-router-dom'
 
-import { Home } from './pages/Home'
+import { Subscribe } from './pages/Subscribe'
 import { Event } from './pages/Event'
 
 export function Router() {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
+      <Route path="/" element={<Subscribe />} />
       <Route path="/event" element={<Event />} />
       <Route path="/event/lesson/:slug" element={<Event />} />
     </Routes>
